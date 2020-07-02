6c1de4068dc9875164b0a1c1e9368007dfb99f16
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2020-07-02 14:06:13 +0900

    초기 커밋.

---
+++ IConparePrac/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IConparePrac
+{
+
+    class Prodect
+    {
+        public int Price { get; set; }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name + " : " + this.Price;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
